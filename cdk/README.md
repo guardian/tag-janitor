a326fe7df70f56e045e2447d523e28abab6fe05d
Jamie Lynch <jamie.lynch@guardian.co.uk>
2020-10-12 14:39:35 +0100

    add cdk

---
+++ cdk/README.md
@@ -0,0 +1,12 @@
+# Tag Janitor Infrastructure
+
+This directory defines the components to be deployed to AWS
+
+## Useful commands
+
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,67 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as s3 from '@aws-cdk/aws-s3';
+import * as iam from '@aws-cdk/aws-iam'
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucketName = new cdk.CfnParameter(this, "BucketName", {
+      type: "String",
+      description: "BucketName"
+    })
+
+    const app = "tag-janitor"
+    
+    const stackParameter = new cdk.CfnParameter(this, "stack", {
+      type: "String",
+      description: "Stack"
+    });
+
+    const stageParameter = new cdk.CfnParameter(this, "stage", {
+      type: "String",
+      description: "Stage"
+    });
+    
+    const topicParameter = new cdk.CfnParameter(this, "topicArn", {
+      type: "String",
+      description: "The ARN of the SNS topic to send messages to"
+    });
+    
+    const accountsAllowListParameter = new cdk.CfnParameter(this, "accountsAllowList", {
+      type: "String",
+      description: "A comma separated list of account numbers to include"
+    });
+
+    const deployBucket = s3.Bucket.fromBucketName(
+      this,
+      "DeployBucket",
+      bucketName.valueAsString
+    );
+
+    const tagJanitorLambda = new lambda.Function(
+      this,
+      `${app}-lambda`,
+      {
+        functionName: `${app}-${stageParameter.valueAsString}`,
+        handler: "dist/src/handler.handler",
+        runtime: lambda.Runtime.NODEJS_12_X,
+        code: lambda.Code.fromBucket(
+          deployBucket,
+          `${stackParameter.valueAsString}/${stageParameter.valueAsString}/${app}/${app}.zip`
+        ),
+        environment: { STAGE: stageParameter.valueAsString, TOPIC_ARN: topicParameter.valueAsString, ACCOUNTS_ALLOW_LIST: accountsAllowListParameter.valueAsString },
+        description: "Lambda to notify about invalid instances",
+        timeout: cdk.Duration.seconds(15),
+        memorySize: 512
+      }
+    );
+
+    const snsPolicyStatment = new iam.PolicyStatement()
+    snsPolicyStatment.addActions("SNS:Publish")
+    snsPolicyStatment.addResources(topicParameter.valueAsString)
+
+    tagJanitorLambda.addToRolePolicy(snsPolicyStatment)
+  }
+}
