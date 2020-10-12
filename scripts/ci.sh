3f2f2984998657547a2a55d8c2315d3c943a92e3
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-10-12 15:32:56 +0100

    Compile Lambda

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,6 +5,7 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOT_DIR="${DIR}/.."
 
+pushd "${ROOT_DIR}/lambda"
 echo "##teamcity[blockOpened name='npm']"
 yarn
 echo "##teamcity[blockClosed name='npm']"
@@ -12,6 +13,12 @@ echo "##teamcity[blockClosed name='npm']"
 echo "##teamcity[testSuiteStarted name='lint']"
 yarn lint
 echo "##teamcity[testSuiteFinished name='lint']"
+
+echo "##teamcity[testSuiteStarted name='compile']"
+yarn compile
+echo "##teamcity[testSuiteFinished name='compile']"
+
+popd
 
 echo "##teamcity[blockOpened name='generate-cfn']"
 "${ROOT_DIR}/scripts/generate-cfn.sh"
