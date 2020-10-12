4c70fa79791d867bde186515f71ab8bcbe0d68f3
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-10-12 15:05:23 +0100

    Start README

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,7 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
-import * as s3 from '@aws-cdk/aws-s3';
-import * as iam from '@aws-cdk/aws-iam'
+import * as s3 from "@aws-cdk/aws-s3";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,29 +9,38 @@ export class CdkStack extends cdk.Stack {
 
     const bucketName = new cdk.CfnParameter(this, "BucketName", {
       type: "String",
-      description: "BucketName"
-    })
+      description: "BucketName",
+    });
 
-    const app = "tag-janitor"
-    
+    const app = "tag-janitor";
+
     const stackParameter = new cdk.CfnParameter(this, "stack", {
       type: "String",
-      description: "Stack"
+      description: "Stack",
     });
 
     const stageParameter = new cdk.CfnParameter(this, "stage", {
       type: "String",
-      description: "Stage"
+      description: "Stage",
     });
-    
+
     const topicParameter = new cdk.CfnParameter(this, "topicArn", {
       type: "String",
-      description: "The ARN of the SNS topic to send messages to"
+      description: "The ARN of the SNS topic to send messages to",
     });
-    
-    const accountsAllowListParameter = new cdk.CfnParameter(this, "accountsAllowList", {
+
+    const accountsAllowListParameter = new cdk.CfnParameter(
+      this,
+      "accountsAllowList",
+      {
+        type: "String",
+        description: "A comma separated list of account numbers to include",
+      }
+    );
+
+    const prismUrl = new cdk.CfnParameter(this, "prismUrl", {
       type: "String",
-      description: "A comma separated list of account numbers to include"
+      description: "Base URL for Prism",
     });
 
     const deployBucket = s3.Bucket.fromBucketName(
@@ -40,28 +49,29 @@ export class CdkStack extends cdk.Stack {
       bucketName.valueAsString
     );
 
-    const tagJanitorLambda = new lambda.Function(
-      this,
-      `${app}-lambda`,
-      {
-        functionName: `${app}-${stageParameter.valueAsString}`,
-        handler: "dist/src/handler.handler",
-        runtime: lambda.Runtime.NODEJS_12_X,
-        code: lambda.Code.fromBucket(
-          deployBucket,
-          `${stackParameter.valueAsString}/${stageParameter.valueAsString}/${app}/${app}.zip`
-        ),
-        environment: { STAGE: stageParameter.valueAsString, TOPIC_ARN: topicParameter.valueAsString, ACCOUNTS_ALLOW_LIST: accountsAllowListParameter.valueAsString },
-        description: "Lambda to notify about invalid instances",
-        timeout: cdk.Duration.seconds(15),
-        memorySize: 512
-      }
-    );
+    const tagJanitorLambda = new lambda.Function(this, `${app}-lambda`, {
+      functionName: `${app}-${stageParameter.valueAsString}`,
+      handler: "dist/src/handler.handler",
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromBucket(
+        deployBucket,
+        `${stackParameter.valueAsString}/${stageParameter.valueAsString}/${app}/${app}.zip`
+      ),
+      environment: {
+        STAGE: stageParameter.valueAsString,
+        TOPIC_ARN: topicParameter.valueAsString,
+        ACCOUNTS_ALLOW_LIST: accountsAllowListParameter.valueAsString,
+        PRISM_URL: prismUrl.valueAsString,
+      },
+      description: "Lambda to notify about invalid instances",
+      timeout: cdk.Duration.seconds(15),
+      memorySize: 512,
+    });
 
-    const snsPolicyStatment = new iam.PolicyStatement()
-    snsPolicyStatment.addActions("SNS:Publish")
-    snsPolicyStatment.addResources(topicParameter.valueAsString)
+    const snsPolicyStatment = new iam.PolicyStatement();
+    snsPolicyStatment.addActions("SNS:Publish");
+    snsPolicyStatment.addResources(topicParameter.valueAsString);
 
-    tagJanitorLambda.addToRolePolicy(snsPolicyStatment)
+    tagJanitorLambda.addToRolePolicy(snsPolicyStatment);
   }
 }
