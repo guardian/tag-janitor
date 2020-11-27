f0f33e2dbef7bb5f2fdd7df80b5a0b843353e17a
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-11-27 10:08:32 +0000

    Add snapshot test and lint step to script

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,22 +1,12 @@
-import {
-  expect as expectCDK,
-  matchTemplate,
-  MatchStyle,
-} from "@aws-cdk/assert";
-import * as cdk from "@aws-cdk/core";
-import * as Cdk from "../lib/cdk-stack";
+import { SynthUtils } from "@aws-cdk/assert";
+import { App } from "@aws-cdk/core";
+import { CdkStack } from "../lib/cdk-stack";
 
-xtest("Empty Stack", () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new Cdk.CdkStack(app, "MyTestStack");
-  // THEN
-  expectCDK(stack).to(
-    matchTemplate(
-      {
-        Resources: {},
-      },
-      MatchStyle.EXACT
-    )
-  );
+describe("The tag-janitor stack", () => {
+  it("matches the snapshot", () => {
+    const app = new App();
+    const role = new CdkStack(app, "tag-janitor");
+
+    expect(SynthUtils.toCloudFormation(role)).toMatchSnapshot();
+  });
 });
