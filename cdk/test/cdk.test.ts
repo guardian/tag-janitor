e51109716bb5ce7e3163d04050edd2cfa2899f24
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-11-27 17:08:37 +0000

    stack, not role!

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -5,8 +5,8 @@ import { CdkStack } from "../lib/cdk-stack";
 describe("The tag-janitor stack", () => {
   it("matches the snapshot", () => {
     const app = new App();
-    const role = new CdkStack(app, "tag-janitor");
+    const stack = new CdkStack(app, "tag-janitor");
 
-    expect(SynthUtils.toCloudFormation(role)).toMatchSnapshot();
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
   });
 });
