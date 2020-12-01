0c246e46455398c34895d633a883ba436af5d8df
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-12-01 14:30:02 +0000

    update snapshot

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,4 +4,4 @@ import { CdkStack } from "../lib/cdk-stack";
 import { App } from "@aws-cdk/core";
 
 const app = new App();
-new CdkStack(app, "TagJanitor");
+new CdkStack(app, "CdkStack");
--- scripts/test-cdk.sh
+++ scripts/test-cdk.sh
@@ -6,7 +6,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOT_DIR="${DIR}/.."
 
 pushd "${ROOT_DIR}/cdk"
-yarn --network-concurrency 1 --frozen-lockfile --silent
+yarn --silent
 yarn lint
 yarn test
 popd
