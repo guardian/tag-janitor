e9e6fad530a6ddc86c4e22575577378920f2c8f2
Akash Askoolum <akash1810@gmail.com>
2021-10-29 15:42:10 +0100

    ci: Use composite GHA provided by GuCDK

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,16 +5,6 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOT_DIR="${DIR}/.."
 
-CDK_OUTPUT_DIR="${ROOT_DIR}/cloudformation/cdk"
-
-(
-  cd "${ROOT_DIR}/cdk"
-   yarn --frozen-lockfile
-   yarn lint
-   yarn test
-   yarn cdk synth -o "${CDK_OUTPUT_DIR}"
-)
-
 (
   cd "${ROOT_DIR}/lambda"
   yarn --frozen-lockfile
