c6c7ad869ee1694938df5fd708962a0dbf24a7ab
Akash Askoolum <akash1810@gmail.com>
2022-07-22 14:08:26 +0100

    chore: Fix linting errors following move to NPM workspaces
    These errors have surfaced since moving to NPM workspaces and using a stricter `tsconfig.json`.

--- packages/lambda/src/handler.ts
+++ packages/lambda/src/handler.ts
@@ -1,105 +1,112 @@
-import axios from "axios";
-import {
-  Anghammarad,
-  NotifyParams,
-  RequestedChannel,
-} from "@guardian/anghammarad";
-import { Accounts, Instance, PrismInstancesResponse } from "./interfaces";
+import type { NotifyParams } from '@guardian/anghammarad';
+import { Anghammarad, RequestedChannel } from '@guardian/anghammarad';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+import type { Accounts, Instance, PrismInstancesResponse } from './interfaces';
 
-require("dotenv").config();
+dotenv.config();
 
 const getTopicArn = (): string => {
-  const arn = process.env.TOPIC_ARN;
-  if (!arn) {
-    throw new Error("No Topic ARN in env");
-  }
-  return arn;
+	const arn = process.env.TOPIC_ARN;
+	if (!arn) {
+		throw new Error('No Topic ARN in env');
+	}
+	return arn;
 };
 
 function createNotifyParams({
-  accountName,
-  message,
-  accountNumber,
-  topicArn,
+	accountName,
+	message,
+	accountNumber,
+	topicArn,
 }: {
-  accountName: string;
-  message: string;
-  accountNumber: string;
-  topicArn: string;
+	accountName: string;
+	message: string;
+	accountNumber: string;
+	topicArn: string;
 }) {
-  return {
-    subject: `AWS account ${accountName} has instances with missing tags`,
-    message: message,
-    target: { AwsAccount: accountNumber },
-    channel: RequestedChannel.Email,
-    topicArn: topicArn,
-    sourceSystem: "tag-janitor",
-    actions: [],
-  };
+	return {
+		subject: `AWS account ${accountName} has instances with missing tags`,
+		message: message,
+		target: { AwsAccount: accountNumber },
+		channel: RequestedChannel.Email,
+		topicArn: topicArn,
+		sourceSystem: 'tag-janitor',
+		actions: [],
+	};
 }
 
 async function getInstancesWithMissingTags(): Promise<Instance[]> {
-  const prismResponse = await axios.get(`${process.env.PRISM_URL}/instances`);
-  const data: PrismInstancesResponse = prismResponse.data;
-  return data.data.instances.filter(
-    (i) => !i.tags["Stack"] || !i.tags["Stage"] || !i.tags["App"]
-  );
+	const prismUrl: string | undefined = process.env.PRISM_URL;
+
+	if (!prismUrl) {
+		throw new Error('PRISM_URL env var not found.');
+	}
+
+	const prismResponse = await axios.get<PrismInstancesResponse>(
+		`${prismUrl}/instances`,
+	);
+	const data: PrismInstancesResponse = prismResponse.data;
+	return data.data.instances.filter(
+		(i) => !i.tags['Stack'] || !i.tags['Stage'] || !i.tags['App'],
+	);
 }
 
 function groupInstancesWithAccounts({
-  instancesWithMissingTags,
-  pAndEAccounts,
+	instancesWithMissingTags,
+	pAndEAccounts,
 }: {
-  instancesWithMissingTags: Instance[];
-  pAndEAccounts?: string[];
+	instancesWithMissingTags: Instance[];
+	pAndEAccounts?: string[];
 }): Accounts {
-  const accounts: Accounts = {};
-  instancesWithMissingTags
-    .filter((instance) =>
-      pAndEAccounts?.includes(instance.meta.origin.accountNumber)
-    )
-    .forEach((instance) => {
-      const { accountNumber } = instance.meta.origin;
-      accounts[accountNumber]
-        ? accounts[accountNumber].push(instance)
-        : (accounts[accountNumber] = [instance]);
-    });
-  return accounts;
+	return instancesWithMissingTags
+		.filter((instance) =>
+			pAndEAccounts?.includes(instance.meta.origin.accountNumber),
+		)
+		.reduce<Accounts>((acc, instance) => {
+			const { accountNumber } = instance.meta.origin;
+			const current = acc[accountNumber] ?? [];
+			return {
+				...acc,
+				[accountNumber]: [...current, instance],
+			};
+		}, {});
 }
 
 export function getFormattedInstanceList(instances: Instance[]) {
-  const tags = ["App", "Stack", "Stage"];
+	const tags = ['App', 'Stack', 'Stage'];
 
-  return instances
-    .map((instance) => {
-      const missingTags = tags.filter((tag) => !instance.tags[tag]).join(", ");
-      const presentTags = tags
-        .filter((tag) => !missingTags.includes(tag))
-        .map((tag) => `${tag}: ${instance.tags[tag]}`)
-        .join(", ");
-      const s = missingTags.length > 1 ? "(s)" : "";
-      const hyperlinkInstanceName = `[${instance.instanceName}](${instance.meta.href})`;
-      const additionalInfo = [
-        instance.specification.instanceType,
-        ...(presentTags ? [presentTags] : []),
-      ].join(", ");
-      return `* **${hyperlinkInstanceName}** (${additionalInfo}) is missing tag${s} **${missingTags}**`;
-    })
-    .join("\n");
+	return instances
+		.map((instance) => {
+			const missingTags = tags.filter((tag) => !instance.tags[tag]).join(', ');
+			const presentTags = tags
+				.filter((tag) => !missingTags.includes(tag))
+				// eslint-disable-next-line @typescript-eslint/restrict-template-expressions -- TODO fix this!
+				.map((tag) => `${tag}: ${instance.tags[tag]}`)
+				.join(', ');
+			const s = missingTags.length > 1 ? '(s)' : '';
+			const hyperlinkInstanceName = `[${instance.instanceName}](${instance.meta.href})`;
+			const additionalInfo = [
+				instance.specification.instanceType,
+				...(presentTags ? [presentTags] : []),
+			].join(', ');
+			return `* **${hyperlinkInstanceName}** (${additionalInfo}) is missing tag${s} **${missingTags}**`;
+		})
+		.join('\n');
 }
 
 export function generateMessage({
-  accountName,
-  accountNumber,
-  instances,
-  formattedInstanceList,
+	accountName,
+	accountNumber,
+	instances,
+	formattedInstanceList,
 }: {
-  accountNumber: string;
-  accountName: string;
-  instances: Instance[];
-  formattedInstanceList: string;
+	accountNumber: string;
+	accountName: string;
+	instances: Instance[];
+	formattedInstanceList: string;
 }): string {
-  return `Hello,
+	return `Hello,
 
 The AWS account ${accountNumber} (**${accountName}**) has ${instances.length} instances missing either Stack, Stage or App tags:
 
@@ -110,50 +117,61 @@ Please update your instances or Cloudformation to include the required tags.
 }
 
 export const handler = async () => {
-  const anghammarad = new Anghammarad();
-  const topicArn = getTopicArn();
-  const pAndEAccounts = process.env.ACCOUNTS_ALLOW_LIST?.split(",");
+	const anghammarad = new Anghammarad();
+	const topicArn = getTopicArn();
+	const pAndEAccounts = process.env.ACCOUNTS_ALLOW_LIST?.split(',');
 
-  const instancesWithMissingTags = await getInstancesWithMissingTags();
-  const accounts: Accounts = groupInstancesWithAccounts({
-    instancesWithMissingTags,
-    pAndEAccounts,
-  });
+	const instancesWithMissingTags = await getInstancesWithMissingTags();
+	const accounts: Accounts = groupInstancesWithAccounts({
+		instancesWithMissingTags,
+		pAndEAccounts,
+	});
 
-  console.log(
-    `Found ${
-      Object.keys(accounts).length
-    } allowed accounts with invalid instances:\n${Object.keys(accounts).join(
-      ", "
-    )}`
-  );
+	console.log(
+		`Found ${
+			Object.keys(accounts).length
+		} allowed accounts with invalid instances:\n${Object.keys(accounts).join(
+			', ',
+		)}`,
+	);
 
-  for (const accountNumber of Object.keys(accounts)) {
-    const instances = accounts[accountNumber];
-    const { accountName } = instances[0].meta.origin;
-    const formattedInstanceList = getFormattedInstanceList(instances);
-    const message = generateMessage({
-      accountNumber,
-      accountName,
-      instances,
-      formattedInstanceList,
-    });
+	for (const accountNumber of Object.keys(accounts)) {
+		const instances = accounts[accountNumber] ?? [];
 
-    const params: NotifyParams = createNotifyParams({
-      accountName,
-      message,
-      topicArn,
-      accountNumber,
-    });
+		if (instances.length === 0) {
+			break;
+		}
 
-    console.log("Notifying Anghamarrad with params", params);
-    const angRes = await anghammarad.notify(params);
-    console.log("Anghammarad response", angRes);
-  }
+		// @ts-expect-error -- TODO fix this!
+		const { accountName } = instances[0].meta.origin;
+		const formattedInstanceList = getFormattedInstanceList(instances);
+		const message = generateMessage({
+			accountNumber,
+			accountName,
+			instances,
+			formattedInstanceList,
+		});
+
+		const params: NotifyParams = createNotifyParams({
+			accountName,
+			message,
+			topicArn,
+			accountNumber,
+		});
+
+		console.log('Notifying Anghamarrad with params', params);
+		const angRes = await anghammarad.notify(params);
+		console.log('Anghammarad response', angRes);
+	}
 };
 
 if (require.main === module) {
-  (async function f() {
-    await handler().catch((err) => console.error(err));
-  })();
+	(async function f() {
+		await handler().catch((err) => console.error(err));
+	})()
+		.then(() => console.log('Done'))
+		.catch((err) => {
+			console.error(err);
+			process.exit(1);
+		});
 }
--- packages/lambda/src/interfaces.ts
+++ packages/lambda/src/interfaces.ts
@@ -1,48 +1,44 @@
-export interface Accounts {
-  [accountNumber: string]: Instance[];
-}
+export type Accounts = Record<string, Instance[]>;
 
 export interface Instance {
-  [data: string]: any;
-  tags: {
-    [key: string]: string;
-  };
-  arn: string;
-  name: string;
-  vendorState: string;
-  group: string;
-  dnsName: string;
-  ip: string;
-  addresses: { public: any[]; private: any[] };
-  createdAt: string;
-  instanceName: string;
-  region: string;
-  vendor: string;
-  securityGroups: string[];
-  app: string[];
-  stack: string;
-  stage: string;
-  mainclasses: [];
-  management: [[Object]];
-  specification: {
-    imageId: string;
-    imageArn: string;
-    instanceType: string;
-    vpcId: string;
-  };
-  meta: {
-    href: string;
-    origin: {
-      accountNumber: string;
-      accountName: string;
-      ownerId: string;
-      region: string;
-    };
-  };
+	[data: string]: unknown;
+	tags: Record<string, string>;
+	arn: string;
+	name: string;
+	vendorState: string;
+	group: string;
+	dnsName: string;
+	ip: string;
+	addresses: { public: unknown[]; private: unknown[] };
+	createdAt: string;
+	instanceName: string;
+	region: string;
+	vendor: string;
+	securityGroups: string[];
+	app: string[];
+	stack: string;
+	stage: string;
+	mainclasses: [];
+	management: Record<string, unknown>;
+	specification: {
+		imageId: string;
+		imageArn: string;
+		instanceType: string;
+		vpcId: string;
+	};
+	meta: {
+		href: string;
+		origin: {
+			accountNumber: string;
+			accountName: string;
+			ownerId: string;
+			region: string;
+		};
+	};
 }
 
 export interface PrismInstancesResponse {
-  data: {
-    instances: Instance[];
-  };
+	data: {
+		instances: Instance[];
+	};
 }
